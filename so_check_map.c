86ad9a5957a033a21676554e34eb0f1e82f91483
adrgonza <105714518+adriiiiiiiii@users.noreply.github.com>
2023-02-24 20:24:15 +0100

    v0.3 map checking more detailed

--- so_check_map.c
+++ so_check_map.c
@@ -1,49 +1,71 @@
 #include "so_long.h"
 
-int ft_check_items(t_map *map) 
+int ft_check_played(t_map *map)
 {
-	int i;
-
-	i = 1;
-	printf("\n%c\n", map->mapita[0][0]);
-	/*
-	while (map->mapita[1][i] == '1' && map->mapita[map->map_height][i] == '1')
-		i++;
-	if (map->mapita[1][++i] != 0 || map->mapita[map->map_height][i] != 0)
-	*/
-		//return 0;
 	return (1);
+}
+
+int ft_check_items(t_map *map) // check if it has only 1 p e
+{
+	char m;
+	int i;
+	int j;
+	int p;
+	int c;
+	int e;
 	
+	p = 0;
+	c = 0;
+	e = 0;
+	i = -1;
+	while (++i < map->map_height)
+	{
+		j = 0;
+		while (j < map->map_weight - 1)
+		{
+			m = map->mapita[i][j];
+			if (map->mapita[i][j] == 'P')
+				p++;
+			if (map->mapita[i][j] == 'C')
+				c++;
+			if (map->mapita[i][j++] == 'E')
+				e++;
+			if (m != '1' && m != '0' && m != 'P' && m != 'C' && m != 'E')
+				return 0;			
+		}
+	}
+	if(!ft_check_played(map) || p != 1 || c < 1 || e != 1)
+		return 0;
+	return (1);	
 }
 
 int ft_read_map(int fd,char *file_name, t_map *map)
 {
 	int i;
 
-	i = 0;
-	while (get_next_line(fd))//check map lines for size of char **
-		i++;
-	map->map_height = i; //giving height map value
 	fd = open(file_name, O_RDONLY);// reopen file
-	map->mapita = malloc(sizeof(char *) * i + 1); //allocate memory for map
+	map->mapita = malloc(sizeof(char *) * map->map_height + 1); //allocate memory for map
+	if(!map->mapita)
+		return 0;
 	map->mapita[i + 1] = NULL; 
 	i = 0;
 	while (i <= map->map_height - 1) //putting map on map variable
-	{
-		map->mapita[i] = get_next_line(fd);
-		
-		i++;
-	}
-	printf("\n%d\n", i); 
+		map->mapita[i++] = get_next_line(fd);
 	i = 0;
 	while (map->mapita[1][++i]); //checking weight
 	map->map_weight = i; // giving weight map value
-	printf("%d\n", i); 
+	i = 0;
+	while (map->mapita[0][i] && map->mapita[map->map_height - 1][i])//check 1  on sides
+		if(map->mapita[0][i] != '1' || map->mapita[map->map_height - 1][i++] != '1')
+			return (0);
+	i = 0;
+	while (i < map->map_height) // same of before
+		if(map->mapita[i][0] != '1' || map->mapita[i++][map->map_weight - 2] != '1')
+			return (0);
 	if (!ft_check_items(map))
 		return 0;
 	return 1;
 }
-//ptr a mapa = malloc(sizeof(t_map))
 
 int ft_check_file(char *file_name, t_map *map)
 {
@@ -64,6 +86,10 @@ int ft_check_file(char *file_name, t_map *map)
 		return 0;
 	if(file_name[--i] != '.')
 		return 0;
+	i = 0;
+	while (get_next_line(fd))//check map lines for size of char **
+		i++;
+	map->map_height = i; //giving height map value
 	if (!ft_read_map(fd, file_name, map))
 		return 0;
 	return (1);
--- so_long.c
+++ so_long.c
@@ -12,7 +12,6 @@ int  ft_image_map(t_map *map, t_game *game)
 		k = 1;
 		while(j < map->map_weight - 1)
 		{
-			mlx_put_image_to_window(game->mlx, game->window, game->floor, k, l);
 			if(map->mapita[i][j] == '0')
 				mlx_put_image_to_window(game->mlx, game->window, game->floor, k, l);
 			if(map->mapita[i][j] == '1')
@@ -40,7 +39,7 @@ int	main(int argc, char **argv)
 	if (argc != 2 || !ft_check_file(argv[1], &map)) //check map
 		return(write(1, "\033[31mError:\nInvalid Map", 23)); 
 	game.mlx = mlx_init(); //start minilib
-	game.window = mlx_new_window(game.mlx, ((map.map_weight - 1) * 32), (map.map_height * 32), "Midlands v0.2"); //create window
+	game.window = mlx_new_window(game.mlx, ((map.map_weight - 1) * 32), (map.map_height * 32), "Midlands v0.3"); //create window
 	game.floor = mlx_xpm_file_to_image(game.mlx, "textures/floor.xpm", &game.image_size, &game.image_size);
 	game.exit = mlx_xpm_file_to_image(game.mlx, "textures/exit.xpm", &game.image_size, &game.image_size);
 	game.player = mlx_xpm_file_to_image(game.mlx, "textures/player.xpm", &game.image_size, &game.image_size);
@@ -48,5 +47,4 @@ int	main(int argc, char **argv)
 	game.wall = mlx_xpm_file_to_image(game.mlx, "textures/wall.xpm", &game.image_size, &game.image_size);
 	ft_image_map(&map, &game);
 	mlx_loop(game.mlx);
-
 }
